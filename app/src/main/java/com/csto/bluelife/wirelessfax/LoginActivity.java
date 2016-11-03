a12f1b9f9b58fa2552b04ca3d0e1b7c31a17bb20
bluelife <28998638@qq.com>
2016-11-04 00:41:46 +0800

    Initial commit

---
+++ app/src/main/java/com/csto/bluelife/wirelessfax/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.csto.bluelife.wirelessfax;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.widget.TextView;
+
+/**
+ * Created by HiWin10 on 2016/11/3.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView textView = (TextView) findViewById(R.id.login_forget);
+        textView.setText(Html.fromHtml(getString(R.string.login_forget_label)));
+
+    }
+}
